1d161a4679fed83a15b69a5846348203c013ece0
arijit3111w <arijitghosh443322@gmail.com>
2024-05-25 15:19:49 +0530

    github added

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
+const gitHub_data={
+  "login": "arijit3111w",
+  "id": 168861779,
+  "node_id": "U_kgDOChCgUw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/168861779?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/arijit3111w",
+  "html_url": "https://github.com/arijit3111w",
+  "followers_url": "https://api.github.com/users/arijit3111w/followers",
+  "following_url": "https://api.github.com/users/arijit3111w/following{/other_user}",
+  "gists_url": "https://api.github.com/users/arijit3111w/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/arijit3111w/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/arijit3111w/subscriptions",
+  "organizations_url": "https://api.github.com/users/arijit3111w/orgs",
+  "repos_url": "https://api.github.com/users/arijit3111w/repos",
+  "events_url": "https://api.github.com/users/arijit3111w/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/arijit3111w/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Arijit Ghosh",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "Making a difference",
+  "twitter_username": null,
+  "public_repos": 2,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2024-05-03T18:41:03Z",
+  "updated_at": "2024-05-25T09:31:14Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -16,7 +50,12 @@ app.get('/login' , (req,res)=>{
 app.get('/youtube' , (req,res)=>{
   res.send('<h1>youtube page</h1>')
 })
+app.get('/github',(req,res)=>{
+  res.json(gitHub_data)
+})
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
